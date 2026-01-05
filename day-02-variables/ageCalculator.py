57eb38c280aa0bd4320d6145a3edabcba373468a
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-05 18:23:21 +0200

    updated the age calculator

--- day-02-variables/ageCalculator.py
+++ day-02-variables/ageCalculator.py
@@ -18,10 +18,14 @@ while True:
 
     age = today.year - birth_date.year
 
+    months_diff = birth_date.month - today.month 
+
+    days_diff = today.day - birth_date.day
+
     if(today.month, today.day) < (birth_date.month, birth_date.day):
         age -= 1
 
-    print(f"Hey {name}, you are currently {age} years old!")
+    print(f"Hey {name}, you are currently {age}, {months_diff} months and {days_diff} days old!")
 
     runAgain = input("run again? (yes/no): ").lower()
     if runAgain != "yes":
