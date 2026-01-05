d4fc6de36b56faac0e6431eb2806cb1b11e8744b
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-05 18:15:05 +0200

    updated the date object

--- day-02-variables/ageCalculator.py
+++ day-02-variables/ageCalculator.py
@@ -1,6 +1,18 @@
-while True:
+from datetime import date
 
-    print("hello buhle");
+print("=== Welcome to My Mini Age Calculator App👯‍♀️ ===")
+
+today = date.today()
+
+while True:
+    
+    name = input("Enter your name: ")
+
+    year = int(input("Enter your birth year: "))
+
+    month = int(input("Enter your birth month(1-12): "))
+
+    day = int(input("Enter your birth date(1-31): "))
 
     runAgain = input("run again? (yes/no): ").lower()
     if runAgain != "yes":
