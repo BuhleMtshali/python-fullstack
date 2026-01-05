f1c4324b7e1f9a783410cc78624afe08e6659c73
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-05 17:29:12 +0200

    updated the python file

---
+++ day-02-variables/ageCalculator.py
@@ -0,0 +1,8 @@
+while True:
+
+    print("hello buhle");
+
+    runAgain = input("run again? (yes/no): ").lower()
+    if runAgain != "yes":
+        print("Thank you for trying my code!")
+        break
