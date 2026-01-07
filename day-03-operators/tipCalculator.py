c2a9ef8f0004a683534852fa461b870241a3173b
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-07 20:52:25 +0200

    updated the tip variables

--- day-03-operators/tipCalculator.py
+++ day-03-operators/tipCalculator.py
@@ -3,7 +3,12 @@ import threading
 print("===== 👾 Mini Tip Calculator 🎰 =====")
 
 def calculate_tip():
-    print("helo....")
+    total_amount = float(input("What is the total of the bill 💵: "))
+    number_of_people = int(input("How many people are Dining 👯‍♀️: "))
+    tip_percentage = float(input("What percentage would you like to give as tip: "))
+
+    #NITTY GRITTY CALCULATIONS
+    total_bill_with_tip = (total_amount * ti)
 
 
 
