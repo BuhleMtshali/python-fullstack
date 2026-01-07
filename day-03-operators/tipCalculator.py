6c6ec2ae45bb66ed2ef02ef43d588fdc65e8db2c
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-07 21:02:49 +0200

    updated the tip calculator

--- day-03-operators/tipCalculator.py
+++ day-03-operators/tipCalculator.py
@@ -3,14 +3,21 @@ import threading
 print("===== 👾 Mini Tip Calculator 🎰 =====")
 
 def calculate_tip():
-    total_amount = float(input("What is the total of the bill 💵: "))
+    total_amount = float(input("What is the total of the bill 💵(R): "))
     number_of_people = int(input("How many people are Dining 👯‍♀️: "))
-    tip_percentage = float(input("What percentage would you like to give as tip: "))
+    tip_percentage = float(input("What percentage would you like to give as tip: ")) * 100
 
     #NITTY GRITTY CALCULATIONS
-    total_bill_with_tip = (total_amount * ti)
+    total_bill_with_tip = (total_amount * tip_percentage) + total_amount
+    amount_each = total_bill_with_tip / number_of_people
 
-
+    print(f"===== 🎰 Bill Summary 🎮 ========")
+    print(f"💵 Total Amount: R{total_amount} ")
+    print(f"Number of people Dining 👯‍♀️: {number_of_people}")
+    print(f"Tip Percentage 🧩: {tip_percentage}%")
+    print(f"Total with Tip 🎲 R:{total_bill_with_tip} ")
+    print(f"Amount Per Person 👾: R{amount_each}")
+    print("======= Thank You ⛳️ ========== ")
 
 #TIMER FOR THE DELAYED FUNCTION
 timer = threading.Timer(3, calculate_tip)
