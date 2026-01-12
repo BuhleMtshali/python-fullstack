65809d01210789cc4e01f9f4dd36de2b16c86287
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-12 15:15:09 +0200

    updated the multiplication generator

---
+++ day-05-loops/multiplication_table.py
@@ -0,0 +1,12 @@
+print("========== 🐝 MINI MULTIPLICATION GENERATOR 🎮 ========")
+
+# STARTING THE WHILE LOOP
+while True:
+    print("loop running...")
+
+
+    #CLOSING THE LOOP
+    runAgain = input("🐝 Wanna generate another table(yes/no): ").lower()
+    if runAgain != "yes":
+        print("===== ⛳️ Thank you For Trying My Mini Calculator 🎲 =====")
+        break
