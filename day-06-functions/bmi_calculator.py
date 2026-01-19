9edf4e6809f664bdbfec39f4a081de2e21ae4ba5
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-19 13:11:16 +0200

    add bmi calculation logic to the calculator function

--- day-06-functions/bmi_calculator.py
+++ day-06-functions/bmi_calculator.py
@@ -3,3 +3,6 @@ def calculate_bmi():
     name = input("🧩Enter your name: ")
     weight = float(input("📋 Enter your weight(kg): "))
     height = float(input("📏 Enter your height(m): "))
+    bmi = weight / (height ** 2)
+    print(f"{name}, Your BMI🎲 : {bmi:.2f}");
+    
