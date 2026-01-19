0f088e87587d2239bf9612333b3226728ab174a4
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-19 13:07:58 +0200

    add bmi calculator function

---
+++ day-06-functions/bmi_calculator.py
@@ -0,0 +1,5 @@
+def calculate_bmi():
+    println("====== 🐝 Welcome to My Mini BMI Calculator 🎲 =======")
+    name = input("🧩Enter your name: ")
+    weight = float(input("📋 Enter your weight(kg): "))
+    height = float(input("📏 Enter your height(m): "))
