071ee9d1b78d93eb7e8f88d852a3d5a426dc6c56
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-21 18:47:37 +0200

    add: implement initial grocery list functionality with timer

--- day-07-list-methods/grocery_list.py
+++ day-07-list-methods/grocery_list.py
@@ -1,1 +1,13 @@
 import threading
+
+print("======= 🐝 Mini Grocery Console Game 🎲 ======")
+
+def grocery_list():
+    print("helo")
+
+
+
+
+#TIMER FOR DELAYED FUNCTION
+timer = threading.Timer(3, grocery_list)
+timer.start()
