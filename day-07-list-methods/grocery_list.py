9d632098d4d49e96478182b7f205aee6f556b08c
Buhle <mtshalizanobuhle7@gmail.com>
2026-02-03 10:28:16 +0200

    refactor: simplify grocery_list function by removing item handling logic

--- day-07-list-methods/grocery_list.py
+++ day-07-list-methods/grocery_list.py
@@ -3,28 +3,14 @@ import threading
 print("======= 🐝 Mini Grocery Console Game 🎲 ======")
 
 def grocery_list():
-    list = []
-
-    #STARTING THE WHILE LOOP
-    while True:
-        items = input("🧩 Enter your first item: ").lower()
-        if len(items) > 2:
-            list.append(items)
-            if len(list) > 0:
-                print("🛍️ Items In Your List: ")
-                for index, item in enumerate(list):
-                    print(f"🧩{index}: {item}")
-            else:
-                print("‼️ List is currently empty!")
-        else:
-            print("‼️INVALID, please add an item first!")
-
-        #CLOSING THE LOOP
-        add_again = input("✨ Wanna Add Another item(yes/no): ").lower()
-        if add_again != "yes":
-            print("====== 🛁 THANK YOU FOR TRYING MY MINI CONSOLE CART 🛍️ =====")
-            break
-
+   while True:
+      print("loop is running...")
+      
+      #CLOSING THE LOOP
+      runAgain = input("👾 Would you like to run another process? (yes/no): ")
+      if runAgain != "yes":
+         print("======== ⏳ Thank you for trying my Mini Console Game 🎮 =========")
+         break;
 
 #TIMER FOR DELAYED FUNCTION
 timer = threading.Timer(3, grocery_list)
