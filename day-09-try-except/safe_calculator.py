2b12f6ebb59bb8527b1c8a152f724cbf4c7b722b
Buhle <mtshalizanobuhle7@gmail.com>
2026-02-11 20:31:39 +0200

    updated the loop

--- day-09-try-except/safe_calculator.py
+++ day-09-try-except/safe_calculator.py
@@ -5,7 +5,7 @@ while True:
     try:
         num1 = float(input("Enter your first number: "))
         num2 = float(input("Enter your second number: "))
-        print(f"Result: {num1} + {num2}")
+        print(f"Result: {num1} + {num2} = {num1 + num2}")
         break
     except ValueError:
         print("Invalid input try again!")
