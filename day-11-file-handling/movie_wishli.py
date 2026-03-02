3ba025cc694aab38bfbc2645b3a9cc27f509c7b1
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-02 19:04:15 +0200

    updated the movie wishlist and created the while loop

--- day-11-file-handling/movie_wishli.py
+++ day-11-file-handling/movie_wishli.py
@@ -1,1 +1,12 @@
 import csv
+
+print("======== 🐝 Welcome to My Mini Movie Wishlist Project ======")
+
+while True:
+    print("loop is runnibg...")
+    
+    
+    addAgain = input("Would you like to add another movie (yes/no): ").lower()
+    if addAgain == "no":
+        print("====== Thank you for Trying My Mini Movie Wishlist =====")
+        break
