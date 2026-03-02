6469f1abef220f11e5f176ede37609fdcbf570bb
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-02 19:12:39 +0200

    updated the movie wishlisty

--- day-11-file-handling/movie_wishli.py
+++ day-11-file-handling/movie_wishli.py
@@ -5,9 +5,12 @@ print("======== 🐝 Welcome to My Mini Movie Wishlist Project ======")
 # CREATING CSV FILE
 with open('movie_list.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-
-while True:
+    writer.writerow(["SN", "Movie Name", "Year"])
     
+while True:
+    ranking_number = int(input("What number is this movie ranking: "))
+    movie_name = input("Enter the movie name: ").lower()
+    movie_year = input("Enter the movie publication year: ")
     
     
     addAgain = input("Would you like to add another movie (yes/no): ").lower()
