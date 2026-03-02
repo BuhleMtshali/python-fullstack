7190732125a3602cedfda76a5b487d4185516915
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-02 18:19:48 +0200

    updated the file

--- day-11-file-handling/file_handling_practice.py
+++ day-11-file-handling/file_handling_practice.py
@@ -9,3 +9,14 @@ print(f"Directory: {os.getcwd()}")
 os.listdir()
 
 #WRITING TO A CSV FILE
+with open('example.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Movie", "Protagonist"])
+    writer.writerow([1, "LOTR", "Frodo"])
+    
+    
+#TRYING TO READ THE FILE
+with open('example.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
