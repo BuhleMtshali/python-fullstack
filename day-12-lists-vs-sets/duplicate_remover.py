e8fa337239fb58225894963d1f0db34f77d871b0
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-05 18:44:21 +0200

    created the main list

--- day-12-lists-vs-sets/duplicate_remover.py
+++ day-12-lists-vs-sets/duplicate_remover.py
@@ -2,10 +2,13 @@ import threading
 
 print("===== 👾 Welcome To My Mini Duplicate Remover 🎮 ====")
 
+#MAIN LIST
+main_list = []
 
 #MAIN FUNCTION
 def check_item():
     print("printing the main function...")
+    
     
     
 #TIMER FOR DELAYED FUNCTION
