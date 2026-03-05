fced7d222640b65a5ecf1e462c19b3056ab1cfec
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-05 18:35:25 +0200

    created the delayed function

--- day-12-lists-vs-sets/duplicate_remover.py
+++ day-12-lists-vs-sets/duplicate_remover.py
@@ -1,3 +1,13 @@
 import threading
 
 print("===== 👾 Welcome To My Mini Duplicate Remover 🎮 ====")
+
+
+#MAIN FUNCTION
+def check_item():
+    print("printing the main function...")
+    
+    
+#TIMER FOR DELAYED FUNCTION
+timer = threading.Timer(3, check_item)
+timer.start()
