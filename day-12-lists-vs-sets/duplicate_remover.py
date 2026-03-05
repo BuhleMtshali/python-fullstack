29d07dafbad316b47416c44846187183a76ba8f8
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-05 19:02:31 +0200

    created the dictionary for the movie

--- day-12-lists-vs-sets/duplicate_remover.py
+++ day-12-lists-vs-sets/duplicate_remover.py
@@ -5,6 +5,17 @@ print("===== 👾 Welcome To My Mini Duplicate Remover 🎮 ====")
 #MAIN LIST
 main_list = []
 
+def add_movie():
+    movie_name = input("🎥 Enter the name of the movie you wanna add to the list: ")
+    movie_publication = input("🎲 When was the movie published?: ")
+    movie_duration = float(input("⌛️ How long is the movie? (in minutes): "))
+    
+    new_movie = {
+        "name": movie_name,
+        "publification": movie_publication,
+        "duration": movie_duration
+    }
+
 #MAIN FUNCTION
 def check_item():
     print("printing the main function...")
