5c74b92af2ea909efc4cf7926696bc384670fe98
Buhle <mtshalizanobuhle7@gmail.com>
2026-05-11 21:44:56 +0200

    updated the toltal

--- day-02-variables/budget_splitter.py
+++ day-02-variables/budget_splitter.py
@@ -6,7 +6,18 @@ while True:
     tip = float(input("How much wold you like to tip(%): "))
     tip_converted = tip / 100
     total_bill_tip = (bill * tip_converted) + bill
-    print(total_bill_tip)
+    person_each = total_bill_tip / people
+    print(f"""
+          ====== 🧾 BILL TIP 🎲 ======
+          
+          Total Bill(excluding tip): R{bill:.2f}
+          Number of people: {people}
+          Tip: {tip}%
+          Total Bill(with tip): R{total_bill_tip:.2f}
+          Person Each: R{person_each:.2f}
+          
+          ======= THANK YOU ========
+          """)
     
     calculateAgain = input("Wanna calculate again(yes/no): ").lower()
     if calculateAgain != "yes":
