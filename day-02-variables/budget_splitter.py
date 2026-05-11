374390a7b243f21fc5e3361e3109f53aed100bb5
Buhle <mtshalizanobuhle7@gmail.com>
2026-05-11 21:26:11 +0200

    UPDATED THE LOOP

--- day-02-variables/budget_splitter.py
+++ day-02-variables/budget_splitter.py
@@ -2,3 +2,12 @@ print("------- 🎲 MINI BUDGET SPLITTER 📝 -------")
 
 bill = float(input("🍛How much is the bill(R): "))
 people = int(input("👯‍♀️How many people are splitting the bill: "))
+
+while True:
+    print("loop running")
+    
+    
+    
+    calculateAgain = input("Wanna calculate again(yes/no): ").lower()
+    if calculateAgain != "yes":
+        print("==== THANK YOU FOR TRYING BUDGET SPLITTER ====")
