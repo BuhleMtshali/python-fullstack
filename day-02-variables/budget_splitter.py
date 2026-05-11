431ff786619c67acef0d8ff85e188833d1d82b2f
Buhle <mtshalizanobuhle7@gmail.com>
2026-05-11 21:32:12 +0200

    updated the calculation

--- day-02-variables/budget_splitter.py
+++ day-02-variables/budget_splitter.py
@@ -3,7 +3,10 @@ print("------- 🎲 MINI BUDGET SPLITTER 📝 -------")
 while True:
     bill = float(input("🍛How much is the bill(R): "))
     people = int(input("👯‍♀️How many people are splitting the bill: "))
-    
+    tip = float(input("How much wold you like to tip(%): "))
+    tip_converted = tip / 100
+    total_bill_tip = (bill * tip_converted) + bill
+    print(total_bill_tip)
     
     calculateAgain = input("Wanna calculate again(yes/no): ").lower()
     if calculateAgain != "yes":
