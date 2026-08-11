50fe771ba41d9ea0fffd5f8e2751a10d1c574f1a
Buhle <mtshalizanobuhle7@gmail.com>
2026-08-11 18:18:36 +0200

    add detailed cart summary display in mini_chart.py

--- day-02-variables/mini_chart.py
+++ day-02-variables/mini_chart.py
@@ -7,3 +7,10 @@ product_name = "bread";
 product_price = 32.50;
 
 product_quantity = 4;
+
+print(f"""
+      ==== 🧺 YOUR CART 🛒 ====
+      Item Name: {product_name};
+      Item Quantity: {product_quantity}
+      Item Price: R{product_price}
+      """);
