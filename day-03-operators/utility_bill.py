23e1406c847206b2bc23ff2ce83d76949a166cf9
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-09 13:10:09 +0200

    refactor utility_bill_calculator function to initialize total_bill and category variables

--- day-03-operators/utility_bill.py
+++ day-03-operators/utility_bill.py
@@ -15,6 +15,14 @@ def utility_bill_calculator():
         units = float(input("💡 Enter units consumed (kWh): "))
         type = int(input("🕹️ Enter connection type (1 for Domestic, 2 for commercial): "))
         
+        #CREATING VARIABLES FOR THE BILL AND CATEGORY
+        total_bill = 0.0
+        category = ""
+        
+        
+        #STARTING ELECTRICITY BILL CALCULATIONS
+        
+        
         #CLOSING THE LOOP
         runagain = input("\n----- 💡 Wanna calculate again? (yes/no): ").lower()
         if runagain != "yes":
